3c63fb05dcc60746aefec914b67ec6940bbb8a91
Dmytro Sereda <sereda21d@gmail.com>
2023-05-10 17:29:54 -0400

    Added references, contact and some js

---
+++ src/js/constants.js
@@ -0,0 +1,16 @@
+export const references = [
+  {
+    name: "Michael Palma",
+    socialUrl: "https://www.linkedin.com/in/palma-tech/",
+    positionName: "Tech Lead",
+    text: `Dima is a standout teammate. A highly talented developer who works hard 
+    at mastering his skillset. He continuously pursues knowledge in various subjects 
+    and uses his experience well to find clear solutions to difficult technical 
+    challenges. He isn't afraid to voice his opinion and regularly gives honest 
+    positive feedback. When things are not clear Dima asks the types of questions 
+    that help the entire team. If you need someone to present or demo a solution 
+    ask Dima because he's an outstanding speaker. He's collaborative, friendly 
+    and always brings a great energy. I wouldn't hesitate in recommending him for 
+    any team.`,
+  },
+];
--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,53 @@
 "use strict";
+import { references } from "./constants";
 
 // DOM SELECTIONS
 const stackElements = document.querySelectorAll(".numbers__item");
+const sliderContainer = document.querySelector(".references__container");
+const sliderArrows = document.querySelectorAll(".arrow");
+const sliderDotsContainer = document.querySelector(".dots__container");
+
+// GENERATE SLIDER
+references.forEach((reference) => {
+  const markup = `<div class="references__item">
+    <div class="references__name-container">
+      <p class="references__name">${reference.name}</p>
+      <a class="references__social-link" target="_blank" href="${
+        reference.socialUrl
+      }">
+        <span
+          class="iconify"
+          data-icon="mdi:linkedin"
+          data-width="16"
+        ></span>
+      </a>
+    </div>
+    <span class="references__position">${reference.positionName}</span>
+    <p class="references__text">
+    ${
+      reference.text.length > 345
+        ? reference.text.slice(0, 345) + "..."
+        : reference.text
+    }
+    </p>
+  </div>`;
+
+  sliderContainer.innerHTML += markup;
+});
 
 // OBSERVER FOR STACK ANIMATION
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      entry.target.classList.add("numbers-show");
+      entry.target.classList.add("show");
     } else {
-      entry.target.classList.remove("numbers-show");
+      entry.target.classList.remove("show");
     }
   });
 });
 
+// OBSERVER SETUP
 stackElements.forEach((element) => observer.observe(element));
+sliderArrows.forEach((arrow) => observer.observe(arrow));
+observer.observe(sliderContainer);
+observer.observe(sliderDotsContainer);
